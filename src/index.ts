71abc451a03b0ba7e61587b4c29d6e61e3bc692e
S.Samarsky <js.samarsky@gmail.com>
2023-05-10 22:13:39 +0300

    fix: port

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, {Request, Response} from 'express'
 const app = express()
 
+const port = process.env.PORT || 5000
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!!!')
 })
 
-app.listen(3000)
+app.listen(port)
